621d35ba9559907e63879b9746a5c0c79c96a477
NikitaGolitsyn <nikita.golitsyn@wesoftyou.com>
2024-08-08 23:43:37 +0300

    Navbar + MobileMenu start

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import MobileMenu from './MobileMenu';
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
-  )
-}
+    <div className="h-24 flex items-center justify-between">
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="font-bold text-xl text-blue-600">
+          NG SOCIAL
+        </Link>
+      </div>
+      <div className="hidden md:flex w-[50%] text-sm">
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2 items-center">
+            <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4" />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2 items-center">
+            <Image src="/friends.png" alt="Friends" width={16} height={16} className="w-4 h-4" />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex gap-2 items-center">
+            <Image src="/stories.png" alt="Stories" width={16} height={16} className="w-4 h-4" />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <MobileMenu />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
