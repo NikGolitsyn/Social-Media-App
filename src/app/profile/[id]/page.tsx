9524984c9ed528cce0b8cf4e8bdddee267a06b7d
NikitaGolitsyn <nikita.golitsyn@wesoftyou.com>
2024-08-21 19:24:55 +0300

    Profile Page > Profile Photo

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Feed from '@/app/components/Feed';
 import LeftMenu from '@/app/components/LeftMenu';
 import RightMenu from '@/app/components/RightMenu';
+import Image from 'next/image';
 
 const ProfilePage = () => {
   return (
@@ -10,6 +11,38 @@ const ProfilePage = () => {
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://images.pexels.com/photos/159884/pexels-photo-159884.jpeg?auto=compress&cs=tinysrgb&w=400"
+                alt=""
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src="https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg?auto=compress&cs=tinysrgb&w=400"
+                alt=""
+                width={128}
+                height={128}
+                className="object-cover w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white z-index-10"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Jaime Torres</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">123</span>
+                <span className="text-sm">Post</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">233</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">322</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
