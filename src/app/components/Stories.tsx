f10faebb70a4a7e387ac5a05b993602694e49c00
NikitaGolitsyn <nikita.golitsyn@wesoftyou.com>
2024-08-21 11:27:54 +0300

    Stories section + scrollbar

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -2,8 +2,86 @@ import Image from 'next/image';
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Eva</span>
+        </div>
+        {/* STORY */}
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image
             src="https://images.pexels.com/photos/27582996/pexels-photo-27582996.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
